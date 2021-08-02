611fd1403ee3b993099c470381650ffb5cfc4a7f
Greeshma <184537@ust-global.com>
2021-08-02 11:01:13 +0530

    my shell scripts

---
+++ dic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+declare -A employee
+employee['ann']='kochi'
+employee['anu']='tana'
+employee['zen']='kochi'
+
+
+while true;
+do
+	echo "1 : Add Employee"
+	echo "2 : Delete Employee"
+	echo "3 : Display all Employee"
+	echo "4 : Search employee"
+	read choice
+	case $choice in
+done
